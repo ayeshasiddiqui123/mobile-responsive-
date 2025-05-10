c9f42f43c053117cc51fb534e2d5685b89fec4b4
ayeshasiddiqui123 <ayeshasiddiqui5656@gmail.com>
2025-05-10 21:11:16 +0500

    Add files via upload

---
+++ e-commerce-mobile-responsive/e-commerce-mobile-responsive/script.js
@@ -0,0 +1,29 @@
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+const menuBtn = document.querySelector('.menu-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.classList.add('open');
+    overlay.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    overlay.classList.remove('show');
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    overlay.classList.remove('show');
+});
+// ------------------------------------first page end-----------------------------
+
+document.querySelectorAll('.remove-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.parentElement.remove();
+    });
+});
+// ------------------------------second page end -----------------------------------
+
+// ----------------------------third page end-------------------------
